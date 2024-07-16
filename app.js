f4c8a0bb95995148482f817c6be0dc179fafc1f9
wcoding <papa5944@gmail.com>
2024-07-16 21:50:17 +0900

    seek local to remote

---
+++ app.js
@@ -0,0 +1,30 @@
+
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+const PORT = process.env.PORT || 2323
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+
+app.listen(PORT,()=> console.log(`Server is listening on port ${PORT}`))
+
+
+module.exports = app;
